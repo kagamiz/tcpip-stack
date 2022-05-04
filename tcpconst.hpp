899b6d9593d8b81ffac97fc6d65c61033a199b08
Jayson Sho Toma <m.kagamiz@gmail.com>
2022-05-04 23:52:59 +0900

    add file to write standard tcp/ip stack constants

---
+++ tcpconst.hpp
@@ -0,0 +1,15 @@
+/**
+ * @file tcpconst.hpp
+ * @author Jayson Sho Toma
+ * @brief Standard constants related to the TCP/IP stack.
+ * @version 0.1
+ * @date 2022-05-04
+ */
+
+#pragma once
+
+ /* Specified in ethernet_hdr->type */
+#define ARP_BROAD_REQ   1
+#define ARP_RPLY        2
+#define ARP_MSG         806
+#define BROADCAST_MAC   0xFFFFFFFFFFFFll
